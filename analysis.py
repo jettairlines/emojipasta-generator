d004effb6e1c3d3f41001976fcbbbeb0bc3304eb
jettairlines <lwanghe2@gmail.com>
2016-04-17 04:13:30 -0500

    kinda working

--- analysis.py
+++ analysis.py
@@ -1,15 +1,20 @@
 import pickle
 import nltk
+import emoji
+from random import randint
 from nltk.stem import *
 from nltk.stem.snowball import SnowballStemmer
 
+## initializers
 stemmer = SnowballStemmer("english")
 emojidata = pickle.load( open( "util/emoji_to_keywords.data", "rb" ) )
 for emo,keys in emojidata.items():
 	keys += nltk.word_tokenize( emo.replace("_"," ") )
 	keys = [stemmer.stem(e) for e in keys]
 
-# print emojidata.items()
+## util functions
+def makeEmo(emo):
+	return ":"+emo+":"
 
 textinput = "Just like I have the Chinese banks in my buildings, they listen to me, they respect me. China has almost complete control over North Korea. China will do that. And if they don't do that, they have to suffer economically because we have the engine that makes China work. You know, without the United States or without China sucking out all our money and our jobs China would collapse in about two minutes."
 # textinput = "panda"
@@ -20,15 +25,16 @@ tokens_stem = [stemmer.stem(e) for e in tokens]
 # print tokens
 
 result = {}
-for inputword in tokens_stem:
+for i,inputword in enumerate(tokens_stem):
 	result[inputword] = []
 	for emo,keys in emojidata.items():
 		for key in keys:
 			if inputword.lower()==key.lower():
-				result[inputword].append(emo);
+				tokens.insert(i,makeEmo(emo));
 
+# print tokens
 
-			
+textoutput = emoji.emojize(" ".join(tokens))
 # textoutput = "".join( ("<i class='em em-" + str(e) + "'></i>" ) for e in result if result[e])
-print result
-# print textoutput
+# print result
+print textoutput
--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from flask import Flask,request, redirect, render_template, send_from_directory, flash, Markup
 import pickle
 import nltk
-from nltk.tokenize import RegexpTokenizer
+import emoji
 from nltk.stem import *
 from nltk.stem.snowball import SnowballStemmer
 application = Flask(__name__)
@@ -15,26 +15,34 @@ def hello():
 def signup():
 	textinput = Markup("<p>"+request.form['textinput']+"</p>")
 
-	tokenizer = RegexpTokenizer(r'\w+')
 	stemmer = SnowballStemmer("english")
 	emojidata = pickle.load( open( "util/emoji_to_keywords.data", "rb" ) )
 	for emo,keys in emojidata.items():
-		keys += tokenizer.tokenize( emo.replace("_"," ") )
+		keys += nltk.word_tokenize( emo.replace("_"," ") )
 		keys = [stemmer.stem(e) for e in keys]
 
-	tokens = tokenizer.tokenize(textinput)
-	tokens = [stemmer.stem(e) for e in tokens]
+	# print emojidata.items()
+
+	textinput = "Just like I have the Chinese banks in my buildings, they listen to me, they respect me. China has almost complete control over North Korea. China will do that. And if they don't do that, they have to suffer economically because we have the engine that makes China work. You know, without the United States or without China sucking out all our money and our jobs China would collapse in about two minutes."
+	# textinput = "panda"
+	textoutput = textinput
+
+	tokens = nltk.word_tokenize(textinput)
+	tokens_stem = [stemmer.stem(e) for e in tokens]
 	# print tokens
 
-	result = []
-	for inputword in tokens:
+	result = {}
+	for i,inputword in enumerate(tokens_stem):
+		result[inputword] = []
 		for emo,keys in emojidata.items():
 			for key in keys:
 				if inputword.lower()==key.lower():
-					result.append(emo)
-				
+					tokens.insert(i,":"+emo+":");
 
-	textoutput = "".join( ("<i class='em em-" + str(x) + "'></i>" ) for x in result)
+	# print tokens
+
+	textoutput = emoji.emojize(" ".join(tokens))
+
 	return render_template('index.html', textoutput = Markup(textoutput) )
 
 if __name__ == "__main__":
