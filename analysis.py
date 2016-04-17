c11f8544ebde4dd530bfb237b25eded6e21d4379
jettairlines <lwanghe2@gmail.com>
2016-04-17 05:05:12 -0500

    pasta generation working well

--- analysis.py
+++ analysis.py
@@ -5,6 +5,8 @@ from random import randint
 from nltk.stem import *
 from nltk.stem.snowball import SnowballStemmer
 
+textinput = "Just like I have the Chinese banks in my buildings, they listen to me, they respect me. China has almost complete control over North Korea. China will do that. And if they don't do that, they have to suffer economically because we have the engine that makes China work. You know, without the United States or without China sucking out all our money and our jobs China would collapse in about two minutes."
+
 ## initializers
 stemmer = SnowballStemmer("english")
 emojidata = pickle.load( open( "util/emoji_to_keywords.data", "rb" ) )
@@ -16,25 +18,29 @@ for emo,keys in emojidata.items():
 def makeEmo(emo):
 	return ":"+emo+":"
 
-textinput = "Just like I have the Chinese banks in my buildings, they listen to me, they respect me. China has almost complete control over North Korea. China will do that. And if they don't do that, they have to suffer economically because we have the engine that makes China work. You know, without the United States or without China sucking out all our money and our jobs China would collapse in about two minutes."
-# textinput = "panda"
-textoutput = textinput
 
 tokens = nltk.word_tokenize(textinput)
 tokens_stem = [stemmer.stem(e) for e in tokens]
 # print tokens
 
 result = {}
+emojilist = []
 for i,inputword in enumerate(tokens_stem):
 	result[inputword] = []
 	for emo,keys in emojidata.items():
 		for key in keys:
 			if inputword.lower()==key.lower():
-				tokens.insert(i,makeEmo(emo));
+				if emo not in emojilist: emojilist.append(makeEmo(emo))
+				for j in range(0,randint(0,4)): 
+					tokens.insert(i,makeEmo(emo));
 
-# print tokens
+## more scrabling and funzies
+for i,e in enumerate(tokens):
+	if randint(0,10)>8:
+		for j in range(0,3):
+			tokens.insert(i,emojilist[randint(0,len(emojilist)-1)])
 
-textoutput = emoji.emojize(" ".join(tokens))
-# textoutput = "".join( ("<i class='em em-" + str(e) + "'></i>" ) for e in result if result[e])
-# print result
-print textoutput
+textoutput = emoji.emojize(" ".join(tokens),use_aliases=True)
+
+print emoji.emojize("".join(emojilist),use_aliases=True)
+# print textoutput
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import Flask,request, redirect, render_template, send_from_directory,
 import pickle
 import nltk
 import emoji
+from random import randint
 from nltk.stem import *
 from nltk.stem.snowball import SnowballStemmer
 application = Flask(__name__)
@@ -15,37 +16,44 @@ def hello():
 def signup():
 	textinput = Markup("<p>"+request.form['textinput']+"</p>")
 
+	## initializers
 	stemmer = SnowballStemmer("english")
 	emojidata = pickle.load( open( "util/emoji_to_keywords.data", "rb" ) )
 	for emo,keys in emojidata.items():
 		keys += nltk.word_tokenize( emo.replace("_"," ") )
 		keys = [stemmer.stem(e) for e in keys]
+	## util functions
+	def makeEmo(emo):
+		return ":"+emo+":"
 
-	# print emojidata.items()
-
-	textinput = "Just like I have the Chinese banks in my buildings, they listen to me, they respect me. China has almost complete control over North Korea. China will do that. And if they don't do that, they have to suffer economically because we have the engine that makes China work. You know, without the United States or without China sucking out all our money and our jobs China would collapse in about two minutes."
-	# textinput = "panda"
-	textoutput = textinput
-
+	
 	tokens = nltk.word_tokenize(textinput)
 	tokens_stem = [stemmer.stem(e) for e in tokens]
 	# print tokens
 
 	result = {}
+	emojilist = []
 	for i,inputword in enumerate(tokens_stem):
 		result[inputword] = []
 		for emo,keys in emojidata.items():
 			for key in keys:
 				if inputword.lower()==key.lower():
-					tokens.insert(i,":"+emo+":");
+					if emo not in emojilist: emojilist.append(makeEmo(emo))
+					for j in range(0,randint(0,3)): 
+						tokens.insert(i,makeEmo(emo));
 
-	# print tokens
+	## more scrabling and funzies
+	for i,e in enumerate(tokens):
+		if randint(0,10)>8:
+			for j in range(0,4):
+				tokens.insert(i,emojilist[randint(0,len(emojilist)-1)])
 
-	textoutput = emoji.emojize(" ".join(tokens))
+	textoutput = emoji.emojize(" ".join(tokens),use_aliases=True)
 
 	return render_template('index.html', textoutput = Markup(textoutput) )
 
 if __name__ == "__main__":
+	application.debug = True
 	application.run(host='0.0.0.0')
 
 application.secret_key = 'bush did 9/11'
