3bd9c53d95564c413b29a21c3a6867e4771295e6
jettairlines <lwanghe2@gmail.com>
2016-04-17 06:16:11 -0500

    more randomness

--- analysis.py
+++ analysis.py
@@ -19,7 +19,6 @@ for emo,keys in emojidata.items():
 def makeEmo(emo):
 	return ":"+emo+":"
 
-
 tokens = nltk.word_tokenize(textinput)
 tokens_stem = [stemmer.stem(e) for e in tokens]
 # print tokens
@@ -39,14 +38,19 @@ for i,inputword in enumerate(tokens_stem):
 					for j in range(0,randint(0,4)): 
 						tokens.insert(i,makeEmo(emo));
 
-# ## more scrabling and funzies
-# for i,e in enumerate(tokens)
-# 	if(randint(0,420)>400)
-		
+## more scrabling and funzies
+if emojilist:
+	emojilist = [makeEmo(e) for e in emojilist]
+	for i,e in enumerate(tokens):
+		if(randint(0,420)>337):
+			for j in range(0,randint(0,3)):
+				tokens.insert(i+randint(0,2),emojilist[randint(0,len(emojilist)-1)])
+
+
 
 # print tokens
-
+emojilist = emoji.emojize("".join(emojilist),use_aliases=True)
 textoutput = emoji.emojize(" ".join(tokens),use_aliases=True)
 # textoutput = "".join( ("<i class='em em-" + str(e) + "'></i>" ) for e in result if result[e])
-# print result
+print emojilist
 print textoutput
